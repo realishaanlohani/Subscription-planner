83586516ec43f8b4d9520364b61096cc4a3dcc39
realishaanlohani <ishaanlohani@gmail.com>
2025-10-05 00:07:26 +0530

    Created sample object

--- subscription-planner.py
+++ subscription-planner.py
@@ -3,12 +3,15 @@ class Account:
         self.username = name
         sub_list = []
         print("Welcome to the Subcription manager application! We allow you to organise, edit and\n enquire your subscriptions in the easiest way possible!")
-        print(f"\nAccount '{self.name}' created. Kindly enter your current subscription list")
+        print(f"\nAccount '{self.username}' created. Kindly enter your current subscription list")
         try: 
-            num_of_subs = int(input(f"How many subs would you like to add?, {self.name}\n"))
+            num_of_subs = int(input(f"How many subs would you like to add?, {self.username}\n"))
             for i, num in enumerate(range(0, num_of_subs)):
                 sinput = input(f"Enter Subscription-{i+1} name: ")
                 sub_list.append(sinput)
+            print(sub_list)
                 
         except:
             print("You have entered an invalid. Please try again!")
+
+sample_account = Account("Ishaan Lohani")
