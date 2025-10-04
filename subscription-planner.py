e2492a7d3413cc83fe87b9d4f6441bca24da2a85
realishaanlohani <ishaanlohani@gmail.com>
2025-10-04 23:50:45 +0530

    edited header-self. Entering username and subscription List.

--- subscription-planner.py
+++ subscription-planner.py
@@ -1,5 +1,8 @@
-class BankAccount:
-    def __init__(self, initialAm, acctName):
-        self.balance = initialAm
-        self.name = acctName
-        print(f"\nAccount '{self.name}' created. \nBalance = ${self.balance:.2f}")
+class Account:
+    def __init__(self, name, sub_list):
+        self.username = name
+        self.sublist = sub_list
+        print("Welcome to the Subcription manager application! We allow you to organise, edit and\n enquire your subscriptions in the easiest way possible!")
+        print(f"\nAccount '{self.name}' created. Kindly enter your current subscription list")
+        num_of_subs = input(f"How many subs would you like to add?, {self.name}\n")
+        
